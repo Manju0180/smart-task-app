8377f7450d894d3fde35a183df8fe0b1a1ee410c
Manju0180 <manjunathkalapur474@gmail.com>
2026-03-09 00:17:45 +0530

    Day-one-layout-development-completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout/layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { TasksComponent } from './features/tasks/tasks.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: LayoutComponent,
+        children:[
+            {path:'',redirectTo:'dashboard',pathMatch:'full'},
+            {path:'dashboard', component:DashboardComponent},
+            {path:'tasks',component:TasksComponent},
+            {path:'projects',component:ProjectsComponent}
+        ]
+    }
+];
