41f21bd5664488e14f5af76854fc54988689e91a
Manju0180 <manjunathkalapur474@gmail.com>
2026-03-29 21:43:43 +0530

    added-addTask-and-changeStatus-methods

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from './task.model';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
+
+  showForm = false;
 
   tasks: Task[]=[
     {
@@ -34,5 +38,47 @@ export class TasksComponent {
       dueDate: '2026-03-15'
     }
   ];
+
+  newTask: any = {
+    title:'',
+    status:'Todo',
+    priority:'Medium',
+    dueDate:''
+  }
+
+  addTask(){
+    const task ={
+      id:Date.now.toString(),
+      title:this.newTask.title,
+      status:this.newTask.status,
+      priority:this.newTask.priority,
+      dueDate:this.newTask.dueDate
+    };
+
+    this.tasks.push(task);
+
+    this.newTask = {
+      title:'',
+      status:'Todo',
+      priority:'Medium',
+      dueDate:''
+    }
+    this.showForm = false;
+
+  }
+
+  changeStatus(task:any){
+    if(task.status == 'Todo'){
+      task.status = 'In Progress';
+    }else if(task.status == 'In Progress'){
+      task.status = 'Done';
+    }else{
+      task.status = 'Todo';
+    }
+  }
+
+  toggleForm(){
+    this.showForm = !this.showForm;
+  }
 
 }
